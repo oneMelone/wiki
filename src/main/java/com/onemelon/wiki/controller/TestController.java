40ba487b15c80f59a2879ed100198d9338b1caa9
oneMelone <1040835678@qq.com>
2022-01-28 15:32:52 +0800

    hot deployment

--- src/main/java/com/onemelon/wiki/controller/TestController.java
+++ src/main/java/com/onemelon/wiki/controller/TestController.java
@@ -1,12 +1,23 @@
 package com.onemelon.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    @Value("${test.hello:TEST}")
+    private String testHello;
+
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World!";
+        return "Hello World!" + testHello;
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name) {
+        return "Hello! Post, " + name;
     }
 }
