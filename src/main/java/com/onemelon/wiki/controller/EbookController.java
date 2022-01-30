ee8652926cf68fc7d1598da216a82a521299dcbc
oneMelone <1040835678@qq.com>
2022-01-30 11:47:44 +0800

    add CommonResp for common response

--- src/main/java/com/onemelon/wiki/controller/EbookController.java
+++ src/main/java/com/onemelon/wiki/controller/EbookController.java
@@ -1,6 +1,7 @@
 package com.onemelon.wiki.controller;
 
 import com.onemelon.wiki.domain.Ebook;
+import com.onemelon.wiki.resp.CommonResp;
 import com.onemelon.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +18,10 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/list")
-    public List<Ebook> list() {
-        return ebookService.list();
+    public CommonResp<List<Ebook>> list() {
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        List<Ebook> list =  ebookService.list();
+        resp.setContent(list);
+        return resp;
     }
 }
