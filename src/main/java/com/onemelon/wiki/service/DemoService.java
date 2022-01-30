838ba3f839d56f8b0516544004c4a84473b1ac52
oneMelone <1040835678@qq.com>
2022-01-30 11:35:07 +0800

    add table ebook, generate code

---
+++ src/main/java/com/onemelon/wiki/service/DemoService.java
@@ -0,0 +1,18 @@
+package com.onemelon.wiki.service;
+
+import com.onemelon.wiki.domain.Demo;
+import com.onemelon.wiki.mapper.DemoMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class DemoService {
+    @Resource
+    private DemoMapper demoMapper;
+
+    public List<Demo> list() {
+        return demoMapper.selectByExample(null);
+    }
+}
