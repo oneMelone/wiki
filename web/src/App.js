58ca0f30ab99d0c056ee903d06900164d4279ec4
oneMelone <1040835678@qq.com>
2022-02-06 09:01:26 +0800

    dev home page

--- web/src/App.js
+++ web/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import "antd/dist/antd.css";
+import { Layout, Menu } from 'antd';
+import MainPage from './screens/main-page'
+
+const { Header, Footer } = Layout;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout>
+        <Header className="header">
+          <div className="logo" />
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+            <Menu.Item key="1">nav 1</Menu.Item>
+            <Menu.Item key="2">nav 2</Menu.Item>
+            <Menu.Item key="3">nav 3</Menu.Item>
+          </Menu>
+        </Header>
+        <MainPage />
+        <Footer style={{ textAlign: 'center' }}>Wiki by OneMelon</Footer>
+      </Layout> 
     </div>
   );
 }
